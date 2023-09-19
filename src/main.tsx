87221ba5ef93634dd23573dc097cce6c431aa892
yossefbanhaim <73604318+yossefbenhaim@users.noreply.github.com>
2023-09-19 08:10:17 +0300

    fix(App): fix path of app ---> App

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 
 import ReactDOM from 'react-dom/client';
-import App from 'components/app';
+import App from 'components/App';
 import client from 'utils/client';
 import React from 'react';
 import './index.css';
